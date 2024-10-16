96f98b92308fd9c133c08b25bb9742774f3523e9
Basil Papadimas <papadimas@protonmail.com>
2024-10-16 18:38:31 +0300

    burnout eyes

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,24 @@
 rm -rf public
-cd content/images/img 
-count=1
-for file in *.jpeg ; do
+cd originals
+for file in *.jpeg; do
     [ -e "$file" ] || continue
-    mv "$file" "$(printf "%d.jpeg" "$count")"
-    ((count++))
+    hash=$(md5sum "$file" | awk '{print $1}')
+    new_name="${hash}.jpeg"
+    if [ ! -e "$new_name" ]; then
+        mv "$file" "$new_name"
+        echo "Renamed $file to $new_name"
+    else
+        echo "Skipped $file, $new_name already exists"
+        rm "$file"
+        echo "Deleted $file"
+    fi
 done
+cd ..
+rclone sync -v originals r2:data/originals
+cd ..
+rm -rf content/images/img/*
+cp originals/*.jpeg content/images/img/
+cd content/images/img
 mogrify -verbose -define jpeg:extent=5MB *.jpeg
 cd ../../..
 hugo --logLevel info --minify
